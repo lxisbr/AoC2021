8895ef963d774b2e58d72c59a14eea54dc73ffaa
lxisbr <lxisbr@gmail.com>
2021-12-01 14:51:11 -0500

    bazelified

---
+++ day_1/python/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+from typing import List
+
+from aocd import get_data 
+
+def times_values_increase(depths: List[int], scale: int = 1) -> int:
+    return sum([1 if depths[i] < depths[i+scale] else 0 for i in range(len(depths)-scale)])
+
+if __name__ == "__main__":
+    input = [int(n) for n in get_data(year=2021, day=1).splitlines()]
+    print(times_values_increase(input))     # Part 1
+    print(times_values_increase(input, 3))  # Part 2
